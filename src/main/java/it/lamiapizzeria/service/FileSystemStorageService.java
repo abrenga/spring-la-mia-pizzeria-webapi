2691d3866aa34bdbe5bb9baf7f2f8093a2eb3384
Netta90 <58398245+Netta90@users.noreply.github.com>
2024-08-24 12:44:12 +0200

    added comment

--- src/main/java/it/lamiapizzeria/service/FileSystemStorageService.java
+++ src/main/java/it/lamiapizzeria/service/FileSystemStorageService.java
@@ -1,114 +1,105 @@
 package it.lamiapizzeria.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
-import it.lamiapizzeria.storage.StorageProperties;
-import org.bouncycastle.util.StoreException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
+
+import it.lamiapizzeria.storage.StorageProperties;
 
 @Service
 public class FileSystemStorageService implements IStorageService {
 
     private final Path rootLocation;
 
-
     @Autowired
     public FileSystemStorageService(StorageProperties properties) {
 
-        if(properties.getLocation().trim().length() == 0){
-           throw new IllegalArgumentException("Location is required");
+        if (properties.getLocation().trim().length() == 0) {
+            throw new IllegalArgumentException("Location is required");
         }
 
         this.rootLocation = Paths.get(properties.getLocation());
     }
 
-
     @Override
-   public void init(){
+    public void init() {
         try {
             Files.createDirectories(rootLocation);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-	public void store(MultipartFile file){
+    public void store(MultipartFile file) {
         try {
             if (file.isEmpty()) {
                 throw new RuntimeException();
             }
             Path destinationFile = this.rootLocation.resolve(
-                            Paths.get(file.getOriginalFilename()))
+                    Paths.get(file.getOriginalFilename()))
                     .normalize().toAbsolutePath();
             if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
-                    throw new RuntimeException();
+                throw new RuntimeException();
             }
             try (InputStream inputStream = file.getInputStream()) {
                 Files.copy(inputStream, destinationFile,
                         StandardCopyOption.REPLACE_EXISTING);
             }
-        }
-        catch (IOException e) {
-                throw new RuntimeException(e);
-        }
-
-    }
-
-    @Override
-	public Stream<Path> loadAll(){
-        try {
-            return Files.walk(this.rootLocation, 1)
-                    .filter(path -> !path.equals(this.rootLocation))
-                    .map(this.rootLocation::relativize);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
 
     @Override
-	public Path load(String filename){
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public Path load(String filename) {
         return rootLocation.resolve(filename);
 
     }
 
-	@Override
-    public Resource loadAsResource(String filename){
+    @Override
+    public Resource loadAsResource(String filename) {
         try {
             Path file = load(filename);
             Resource resource = new UrlResource(file.toUri());
             if (resource.exists() || resource.isReadable()) {
                 return resource;
-            }
-            else {
+            } else {
                 throw new RuntimeException();
             }
-        }
-        catch (MalformedURLException e) {
-           throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
 
-    }
+        }
     }
 
     @Override
-	public void deleteAll(){
-            FileSystemUtils.deleteRecursively(rootLocation.toFile());
-        }
-
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
     }
-    
 }
